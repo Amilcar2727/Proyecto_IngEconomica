a1cbb9638adad70ccfd8f86b59657f3eb9bcfdc2
Amilcar <bambino.amil.27@gmail.com>
2024-09-07 18:38:22 -0500

    Agregando Librerias y PseudoModelo del programa

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+from tkinter import *
+from tkinter import ttk;
+def MenuPrincipal():
+    return;
+
 def main():
+    """
+    Valor Presente/Capital (Ci/Co o P)
+    Tasa de Interés (r o i %)
+    Periodo/Tiempo (n)
+    Interés (I) = P*i*n
+    Valor Futuro/Capital Futuro (Cf o F) = P+I
+    """
+    #Menu
+    #Matematicas Financieras
+    #Seleccione el Proceso a realizar:
+    #Apartado desglosable ->
+    #Seleccionar
+    #Salir
+    ### Interés Simple
+    ## -> Ingrese Capital (S./1000 por defecto)
+    ## -> Ingrese Tasa de Interés (0% por defecto)
+    ## -> Ingrese Periodo (1 año por defecto)
+    ## -> Apartado para mostrar Interés Acumulado
+    ## -> Apartado para mostrar Capital Futuro
+    ## -> Apartado para mostrar talvez gráficas
     print();
 
 if __name__ == "__main__":
