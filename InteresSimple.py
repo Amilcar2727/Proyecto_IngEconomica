b59646f04be055cbbba8deee77392ff1f3b8bbf3
Amilcar <bambino.amil.27@gmail.com>
2024-09-09 05:06:21 -0500

    Agregados Validacion de Ingreso de Datos

--- InteresSimple.py
+++ InteresSimple.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from Operaciones import OperacionesMF
+import time;
+#Verificacion
+import re;
 
 class InterfazInteresSimple:
     def __init__(self,root):
@@ -11,25 +14,27 @@ class InterfazInteresSimple:
         self.tituloLb = ttk.Label(self.root, text="-> Calculo de Interés Simple <-");
         self.tituloLb.grid(column=1,row=1,columnspan=2,sticky=(N,W));
         
+        self.check_cap_wrapper = (self.root.register(self.Check_Cap),"%P");
+        self.check_num_wrapper = (self.root.register(self.Check_Num),"%P");
         #Lectura Capital
         self.capitalInicTxt = StringVar();
         self.capitalLb = ttk.Label(self.root, text="Capital en Soles (S/.):");
         self.capitalLb.grid(column=1,row=2,columnspan=2,sticky=W);
-        self.capital_leer = ttk.Entry(self.root,textvariable=self.capitalInicTxt);
+        self.capital_leer = ttk.Entry(self.root,textvariable=self.capitalInicTxt,validate="key",validatecommand=self.check_cap_wrapper);
         self.capital_leer.grid(column=1,row=3,columnspan=2,sticky=W);
         
         #Lectura Tasa de Interez:
         self.tasaTxt = StringVar();
         self.tasaIntLb = ttk.Label(self.root, text="Tasa de Interés (%):");
         self.tasaIntLb.grid(column=1,row=4,columnspan=2,sticky=W);
-        self.tasaInt_leer = ttk.Entry(self.root,textvariable=self.tasaTxt);
+        self.tasaInt_leer = ttk.Entry(self.root,textvariable=self.tasaTxt,validate="key",validatecommand=self.check_num_wrapper);
         self.tasaInt_leer.grid(column=1,row=5,columnspan=2,sticky=W);
         ## => Botones Seleccionables
         #Lectura Periodo:
         self.periodoTxt = StringVar();
         self.periodoLb = ttk.Label(self.root, text="Periodo/Tiempo (años):");
         self.periodoLb.grid(column=1,row=7,columnspan=2,sticky=W);
-        self.periodo_leer = ttk.Entry(self.root,textvariable=self.periodoTxt);
+        self.periodo_leer = ttk.Entry(self.root,textvariable=self.periodoTxt,validate="key",validatecommand=self.check_num_wrapper);
         self.periodo_leer.grid(column=1,row=8,columnspan=2,sticky=W);
         ## => Botones Seleccionables
         
@@ -54,15 +59,31 @@ class InterfazInteresSimple:
         self.volverMenuBt = ttk.Button(self.root, text="Volver Menu",command=self.VolverMenu)
         self.volverMenuBt.grid(column=2,row=10,columnspan=1,sticky=(W,S));
         
+    #Validar solo entrada numerica
+    def Check_Cap(self,newval):
+        return re.match('^[0-9]*$', newval) is not None and len(newval)<=5;
+    def Check_Num(self,newval):
+        return re.match('^[0-9]*$', newval) is not None and len(newval)<=2;
+    
     def CalcularResultados(self):
-        self.interesText.set(self.CalcularInteresSimple());
-        self.capitalFText.set(self.CalcularCapitalFuturo());
+        print(f"->{self.capitalInicTxt.get()},{self.tasaTxt.get()},{self.periodoTxt.get()}");
+        print(f"{type(self.capitalInicTxt)}");
+        if((self.capitalInicTxt.get()=="" or self.capitalInicTxt == None) or
+           (self.tasaTxt.get()=="" or self.tasaTxt == None) or
+           (self.periodoTxt.get()=="" or self.periodoTxt == None)):
+            datosBlancos = ttk.Label(self.root, text="Por favor, rellena todos los espacios");
+            datosBlancos.grid(column=1,row=11,columnspan=2,sticky=S);
+            self.root.after(5000,datosBlancos.destroy);
+            return;
+        interesT = self.CalcularInteresSimple();
+        self.interesText.set(interesT);
+        self.capitalFText.set(self.CalcularCapitalFuturo(interesT));
         
     def CalcularInteresSimple(self):
         resultado = OperacionesMF.InteresAcumulado(self,self.capitalInicTxt.get(),self.tasaTxt.get(),self.periodoTxt.get());
         return resultado;
-    def CalcularCapitalFuturo(self):
-        resultado = OperacionesMF.CapitalFuturo(self,self.capitalInicTxt.get(),self.interesText.get());
+    def CalcularCapitalFuturo(self,interesT):
+        resultado = OperacionesMF.CapitalFuturo(self,self.capitalInicTxt.get(),interesT);
         return resultado;
     
     def VolverMenu(self):
--- Operaciones.py
+++ Operaciones.py
@@ -1,5 +1,19 @@
 class OperacionesMF:
-    def InteresAcumulado(self,capital,tazaInteres,periodo):    
-        return capital*(tazaInteres/10)*periodo;
+    def InteresAcumulado(self,capital,tazaInteres,periodo):   
+        try:
+            capital = int(capital);
+            tazaInteres = float(tazaInteres);
+            periodo = int(periodo);
+            
+            return capital*(tazaInteres/100)*periodo;
+        except ValueError:
+            pass;
+        
+        
     def CapitalFuturo(self,capital,interes):
-        return capital+interes;
+        try:
+            capital = int(capital);
+            interes = int(interes);
+            return capital+interes;
+        except ValueError:
+            pass;
