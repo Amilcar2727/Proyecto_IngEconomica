0a9c8353c5ea89e05f8458325e346349c82c825c
Amilcar <bambino.amil.27@gmail.com>
2024-09-21 21:28:03 -0500

    Agregados métodos para calculo de Interés Compuesto

--- Operaciones.py
+++ Operaciones.py
@@ -1,5 +1,5 @@
 class OperacionesMF:
-    def InteresAcumulado(self,capital,tazaInteres,periodo):   
+    def InteresSimpleAcumulado(self,capital,tazaInteres,periodo):   
         try:
             capital = float(capital);
             tazaInteres = float(tazaInteres);
@@ -9,11 +9,28 @@ class OperacionesMF:
         except ValueError:
             pass;
         
-        
-    def CapitalFuturo(self,capital,interes):
+    def CapitalSimpleFuturo(self,capital,interes):
         try:
             capital = float(capital);
             interes = float(interes);
             return capital+interes;
+        except ValueError:
+            pass;
+        
+    def InteresCompuestoAcumulado(self,capital,tazaInteres,periodo):   
+        try:
+            capital = float(capital);
+            tazaInteres = float(tazaInteres);
+            periodo = float(periodo);
+            
+            return capital*(((tazaInteres+1)**periodo)-1);
+        except ValueError:
+            pass;
+        
+    def CapitalCompuestoFuturo(self,capital,interes,periodo):
+        try:
+            capital = float(capital);
+            interes = float(interes);
+            return capital*((1+interes)**periodo);
         except ValueError:
             pass;
